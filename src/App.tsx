d609ad814f5e5dc8f5e28d6a86f09b7a49d1c869
Prabhav Makkar <65402260+prabhavmakkar@users.noreply.github.com>
2026-04-04 22:01:54 +0530

    feat: add Story timeline section with staggered scroll animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
+import Story from './components/Story'
 
 export default function App() {
   return (
     <div className="bg-white text-black">
       <Nav />
       <Hero />
+      <hr className="mx-auto max-w-5xl border-gray-100" />
+      <Story />
     </div>
   )
 }
